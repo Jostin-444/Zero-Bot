2cbaa700b31b2f6d1261fc02ef583e3b33959430
Jostin-444 <gemersport7@gmail.com>
2024-11-25 13:07:29 -0600

    Actualizar owner-unbanUser.js

--- plugins/owner-unbanUser.js
+++ plugins/owner-unbanUser.js
@@ -38,7 +38,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['unban @user']
 handler.tags = ['owner']
-handler.command = ['unban'] 
+handler.command = ['unban', 'desmute'] 
 handler.rowner = true
 
 export default handler
