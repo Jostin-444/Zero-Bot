2d31a598e7e62b447e5dd06c42b5321b47304cd9
Jostin-444 <gemersport7@gmail.com>
2024-11-25 13:13:10 -0600

    Actualizar owner-banUser.js

--- plugins/owner-banUser.js
+++ plugins/owner-banUser.js
@@ -38,7 +38,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 }
 handler.help = ['ban @user']
 handler.tags = ['owner']
-handler.command = ['ban', 'mute'] 
+handler.command = ['ban'] 
 handler.rowner = true
 
 export default handler
