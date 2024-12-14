ca254f0cc65e42f7d10116ef3e8d1bd42751b537
Jostin-444 <gemersport7@gmail.com>
2024-12-14 11:59:19 -0600

    Add files via upload

--- plugins/ff-reglaslideres2.js
+++ plugins/ff-reglaslideres2.js
@@ -2,8 +2,8 @@ let handler = async (m, { isPrems, conn }) => {
 let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
-let img = 'https://qu.ax/GBxS.jpg' 
-let texto = `» 𝙍𝙀𝙂𝙇𝘼𝙎 𝘾𝙇𝙆 ⭐`
+let img = 'https://telegra.ph/file/2f2d54f056e716d68d4d9.jpg' 
+let texto = `» 𝙍𝙀𝙂𝙇𝘼𝙎 𝙇𝙄𝘿𝙀𝙍𝙀𝙎 𝘽𝘼𝙎𝙄𝘾𝘼𝙎 *2*`
 
 const fkontak = {
 	"key": {
@@ -22,9 +22,11 @@ const fkontak = {
 await conn.sendFile(m.chat, img, 'img.jpg', texto, fkontak)
 global.db.data.users[m.sender].lastcofre = new Date * 1
 }
-handler.help = ['reglasclk']
+handler.help = ['reglaslideres2']
 handler.tags = ['freefire']
-handler.command = ['reglasclk' ,'clk'] 
+handler.command = ['reglaslideres2'] 
 handler.register = true
 handler.admin = true
 export default handler
+
+
