21176ad98f4a8327b60c5477dc3e29eeffac99f4
Jostin-444 <gemersport7@gmail.com>
2024-12-14 11:45:25 -0600

    Update menu-ff.js

--- plugins/menu-ff.js
+++ plugins/menu-ff.js
@@ -3,7 +3,7 @@ let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //8
 if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
 
 let img = 'https://i.ibb.co/1GWf4x8/Sylph.jpg' 
-let texto = `> 𝙈𝙀𝙉𝙐 𝘿𝙀 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ⚔. 
+let texto = `𝙈𝙀𝙉𝙐 𝘿𝙀 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ⚔. 
  
 ★ 𝙁𝙍𝙀𝙀 𝙁𝙄𝙍𝙀 ★
 💜 ➺ 🎫.donarsala
